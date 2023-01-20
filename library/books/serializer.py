4b8ab735797c6fbed08c69d45327fa309ce51393
Mikhail 124rfew <derevgenii@yandex.ru>
2023-01-20 14:15:32 +0300

    up

--- library/books/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-class Serializer(serializers.Serializer):
-	id = serializers.IntegerField()
-	name_book = serializers.CharField(max_length=50)
-	autor = serializers.CharField()
-	date = serializers.DateField()
-	category_id = serializers.IntegerField()
